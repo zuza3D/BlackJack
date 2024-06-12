a7d30380b33b588f37c01e2bca641e798c4f9f3e
Dorota <dm.ptasznik@gmail.com>
2024-06-12 19:44:13 +0200

    Added statistics layout

---
+++ gui/plot_generator.py
@@ -0,0 +1,63 @@
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+
+
+class PlotGenerator:
+    def __init__(self, statistics):
+        self._data = statistics
+        self._histogram_path = 'images/stats/histogram.png'
+        self._pie_chart_path = 'images/stats/pie_chart.png'
+
+    @property
+    def histogram_path(self):
+        return self._histogram_path
+
+    @property
+    def pie_chart_path(self):
+        return self._pie_chart_path
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, new_stats):
+        self._data = new_stats
+
+    def get_game_result(self, tie=True):
+        if tie:
+            return {key: self._data[key] for key in self._data if key != 'balance'}
+        return {key: self._data[key] for key in self._data if key != 'balance' and key != 'tie'}
+
+    def generate_game_result_histogram(self):
+        bar_color = (38 / 255, 3 / 255, 21 / 255, 1)
+        background_color = (240 / 255, 215 / 255, 135 / 255, 1)
+        game_result = self.get_game_result()
+
+        fig, ax = plt.subplots()
+        ax.set_facecolor(background_color)
+        ax.bar(list(game_result.keys()), game_result.values(), color=bar_color)
+        ax.tick_params(axis='both', which='major', labelsize=20, colors=background_color)
+        plt.savefig(self._histogram_path, transparent=True)
+        plt.close()
+
+    def generate_game_result_pie_graph(self):
+        game_result = self.get_game_result(tie=False)
+        labels = game_result.keys()
+        sizes = game_result.values()
+        colors = [(38 / 255, 3 / 255, 21 / 255, 1), (240 / 255, 215 / 255, 135 / 255, 1)]
+
+        fig, ax = plt.subplots()
+        wedges, texts, autotexts = ax.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90)
+        idx = 1
+        for text in autotexts:
+            text.set_color(colors[idx % 2])
+            text.set_fontsize(18)
+            idx += 1
+        for text in texts:
+            text.set_color((240 / 255, 215 / 255, 135 / 255, 1))
+            text.set_fontsize(20)
+            idx += 1
+        plt.savefig(self._pie_chart_path, transparent=True)
+        plt.close()
---
+++ gui/stats_layout.py
@@ -0,0 +1,61 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.widget import Widget
+
+from gui.plot_generator import PlotGenerator
+from gui.widgets import CustomButton, CustomLabel
+
+
+class StatsLayout(GridLayout):
+    def __init__(self, screen_manager, player_stats, **kwargs):
+        super(StatsLayout, self).__init__(**kwargs)
+        self.screen_manager = screen_manager
+        self.player_stats = player_stats
+        self.plot_generator = PlotGenerator(player_stats.stats)
+
+        self.cols = 1
+
+        self.navigation_bar = StackLayout(size_hint=(1, 0.2))
+
+        self.back_button = CustomButton(text="menu".upper(), size_hint=(0.2, 1))
+        self.back_button.bind(on_press=self.back_to_menu)
+        self.navigation_bar.add_widget(self.back_button)
+
+        # self.refresh_button = CustomButton(text="refresh".upper(), size_hint=(0.2, 1))
+        # self.refresh_button.bind(on_press=self.refresh_plots)
+        # self.navigation_bar.add_widget(self.refresh_button)
+        self.navigation_bar.add_widget(Widget(size_hint=(0.2, 1)))
+        self.navigation_bar.add_widget(CustomLabel(text="Statistics".upper(), size_hint=(0.2, 1)))
+        self.add_widget(self.navigation_bar)
+
+        self.plot_panel = self.create_plot_panel()
+        self.update_plot_panel()
+
+    def refresh_plots(self, _):
+        self.update_plot_panel()
+
+    def back_to_menu(self, _):
+        self.screen_manager.current = 'menu'
+
+    def create_plot_panel(self):
+        plot_panel = BoxLayout()
+        plot_panel.orientation = 'horizontal'
+        plot_panel.padding = 20
+        plot_panel.spacing = 20
+        return plot_panel
+
+    def update_plot_panel(self):
+        self.plot_panel.clear_widgets()
+        player_stats_data = self.player_stats.stats
+        self.plot_generator.data = player_stats_data
+
+        self.plot_generator.generate_game_result_histogram()
+        self.plot_generator.generate_game_result_pie_graph()
+
+        self.plot_panel.add_widget(Image(source=self.plot_generator.pie_chart_path))
+        self.plot_panel.add_widget(Image(source=self.plot_generator.histogram_path))
+
+        if self.plot_panel not in self.children:
+            self.add_widget(self.plot_panel)
